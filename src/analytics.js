dde82894fba7228a9623e879efd0089e682ae450
lvqi <161336747+0416ABCD@users.noreply.github.com>
2026-06-17 10:54:26 +0800

    fix: support Novus trackAgent queue method

--- src/analytics.js
+++ src/analytics.js
@@ -36,7 +36,7 @@ function installPendoSnippet(apiKey) {
     let z;
     o = p[d] = p[d] || {};
     o._q = o._q || [];
-    v = ["initialize", "identify", "updateOptions", "pageLoad", "track"];
+    v = ["initialize", "identify", "updateOptions", "pageLoad", "track", "trackAgent"];
     for (w = 0, x = v.length; w < x; ++w) {
       (function queueMethod(method) {
         o[method] =
